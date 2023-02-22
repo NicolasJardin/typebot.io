d52051074665c33e449ef05ba118c06acd46ec67
nicolas-1672953028 <nicolasjdev@gmail.com>
2023-02-22 08:21:21 -0300

    feat: Adicionar nova coluna de tags no banco de dados

--- apps/builder/src/features/blocks/logic/tag/components/TagNodeContent/TagNodeContent.tsx
+++ apps/builder/src/features/blocks/logic/tag/components/TagNodeContent/TagNodeContent.tsx
@@ -1,10 +1,16 @@
+import { useTypebot } from '@/features/editor'
 import { Text } from '@chakra-ui/react'
 import { TagOptions } from 'models'
+import { byId } from 'utils'
 
 type TagNodeContentProps = { options: TagOptions }
 
 export default function TagNodeContent({ options }: TagNodeContentProps) {
+  const { typebot } = useTypebot()
+
+  const variableName = typebot?.tags.find(byId(options.id))?.name
+
   return (
-    <Text color={'gray.500'}>{options.name || 'Clique para editar...'}</Text>
+    <Text color={'gray.500'}>{variableName || 'Clique para editar...'}</Text>
   )
 }
