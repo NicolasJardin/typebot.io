f0043cace1cef13bdb744e3626ac8269bdd0db94
nicolas-1672953028 <nicolasjdev@gmail.com>
2023-02-23 16:26:24 -0300

    feat: Adicionar novas rotas de departamentos e atendentes

--- apps/builder/src/services/api/transfer/useTransfer.ts
+++ apps/builder/src/services/api/transfer/useTransfer.ts
@@ -1,24 +1,44 @@
-import { useCallback } from 'react'
+import { getCookie } from 'cookies-next'
+import jwt_decode from 'jwt-decode'
+import { useCallback, useMemo } from 'react'
 import { instance } from '../base/instance'
+import { AuthJwt } from '../base/interfaces/AuthJwt'
 import { AttendantGetResponse } from './interfaces/AttendantGetResponse'
 import { DepartmentGetResponse } from './interfaces/DepartmentGetResponse'
 
 export default function useTransfer() {
+  const jwt = getCookie('authJwt')
+
+  const companyUuid = useMemo(
+    () =>
+      typeof jwt === 'string'
+        ? jwt_decode<AuthJwt>(jwt).companyUuid
+        : undefined,
+    [jwt]
+  )
+
   const getDepartments = useCallback(
     async () =>
-      (await instance.get<DepartmentGetResponse[]>('ts-api/servicesector/list'))
-        .data,
-    []
+      (
+        await instance.get<DepartmentGetResponse[]>('find-all-sectors', {
+          params: {
+            companyUuid,
+          },
+        })
+      ).data,
+    [companyUuid]
   )
 
   const getAttendants = useCallback(
     async () =>
       (
-        await instance.get<AttendantGetResponse[]>(
-          'attendantController/listOfDatatableAttendants'
-        )
+        await instance.get<AttendantGetResponse[]>('find-all-attendants', {
+          params: {
+            companyUuid,
+          },
+        })
       ).data,
-    []
+    [companyUuid]
   )
 
   return {
