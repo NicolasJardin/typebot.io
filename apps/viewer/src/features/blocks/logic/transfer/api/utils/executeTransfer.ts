bd08c8ee00afee17071aecdf26df752ae097f9e2
nicolas-1672953028 <nicolasjdev@gmail.com>
2023-03-26 09:22:57 -0300

    fix: ajustar build

--- apps/viewer/src/features/blocks/logic/transfer/api/utils/executeTransfer.ts
+++ apps/viewer/src/features/blocks/logic/transfer/api/utils/executeTransfer.ts
@@ -9,20 +9,20 @@ export const executeTransfer = async (
 ): Promise<ExecuteLogicResponse> => {
   //TODO Não está mais caindo aqui por causa das messages no executeGroup
 
-  if (!block.options?.department.id && !block.options?.attendant.id)
+  if (!block.options?.department?.id && !block.options?.attendant?.id)
     return { outgoingEdgeId: block.outgoingEdgeId }
 
   const parsedMessage = parseVariables(variables)(block.options?.message)
 
   const getTransfer = () => {
-    if (block.options?.attendant.id)
+    if (block.options?.attendant?.id)
       return {
         attendantId: block.options?.attendant.id,
         message: parsedMessage,
       }
 
     return {
-      departmentId: block.options?.department.id,
+      departmentId: block.options?.department?.id,
       message: parsedMessage,
     }
   }
@@ -30,7 +30,7 @@ export const executeTransfer = async (
   return {
     outgoingEdgeId: block.outgoingEdgeId,
     clientSideActions:
-      block.options?.attendant.id || block.options?.department.id
+      block.options?.attendant?.id || block.options?.department?.id
         ? [
             {
               transfer: getTransfer(),
