336fb34500f6827a4ada228e27668d948cc79b80
Nicolas Jardin <nicolas.jardin@moovin.com.br>
2023-06-13 18:48:30 -0300

    feat: Quebrar login e sempre utilizar usuario com o id do authId

---
+++ apps/builder/src/features/auth/helpers/getUser.ts
@@ -0,0 +1,17 @@
+import prisma from '@/lib/prisma'
+import { getCookie } from 'cookies-next'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = getCookie('authId', { req, res }) as string | undefined
+
+  if (!id) return null
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id,
+    },
+  })
+
+  return user
+}
