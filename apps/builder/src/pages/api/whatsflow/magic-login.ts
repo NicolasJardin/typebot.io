54a3683ab68b0f508998a6ba648406e0a4170195
Joao Pedro <devjpe@gmail.com>
2023-12-05 07:24:00 -0300

    Test

--- apps/builder/src/pages/api/whatsflow/magic-login.ts
+++ apps/builder/src/pages/api/whatsflow/magic-login.ts
@@ -15,7 +15,6 @@ export default async function handler(
     httpOnly: true,
     secure: true,
     domain: domain,
-    path: '/',
   }
 
   const cookie1 = serialize('authId', authWfID, cookieConfig)
