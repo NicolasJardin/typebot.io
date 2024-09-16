2225abc62c01dd437e0e43b82c768b4a52b702ce
nicolasmoovin <nicolas.jardin@moovin.com.br>
2024-09-16 19:41:42 -0300

    feat: Alterar endpoint de devices no bloco de enviar de

---
+++ apps/builder/src/pages/api/whatsflow/integrations.ts
@@ -0,0 +1,35 @@
+import { instance } from '@/whatsflow/api/base/instance'
+import { AuthJwt } from '@/whatsflow/api/base/interfaces/AuthJwt'
+import { DevicesGetResponse } from '@/whatsflow/api/template/types/DevicesGetResponse'
+import jwt_decode from 'jwt-decode'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const authJwt = req.cookies['authJwt']
+
+  const jwtDecoded = authJwt ? jwt_decode<AuthJwt>(authJwt) : undefined
+
+  const getIntegrations = async () => {
+    try {
+      const data = (
+        await instance.get<DevicesGetResponse>('find-all-integration', {
+          headers: jwtDecoded
+            ? {
+                companyId: jwtDecoded.companyUuid,
+                Authorization: `Bearer ${jwtDecoded.token}`,
+              }
+            : undefined,
+        })
+      ).data
+
+      return data
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  res.status(200).json(await getIntegrations())
+}
